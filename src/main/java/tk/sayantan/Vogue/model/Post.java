ca2b20c1c5750905664eec498154dd0110f2fe43
Sayantan Banerjee <sayantanbanerjee@outlook.com>
2020-07-18 17:41:41 +0530

    Update Post.java

--- src/main/java/tk/sayantan/Vogue/model/Post.java
+++ src/main/java/tk/sayantan/Vogue/model/Post.java
@@ -16,29 +16,33 @@ import java.util.Date;
 
 @Entity
 public class Post {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @NotNull
-    private Long id;
-    @Column(name = "title")
-    private String postName;
-    @Column(name = "post_date")
-    private Date postDate;
-    @Column(name = "last_edit")
-    private Date lastEdit;
-    @Column(name = "content")
-    @Type(type = "text")
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@NotNull
+	private Long id;
+	
+	@Column(name="title")
+	private String postName;
+	@Column(name="post_date")
+	private Date postDate;
+	@Column(name="last_edit")
+	private Date lastEdit;
+	@Column(name="content")
+	@Type(type="text")
     private String postContent;
     @Column(name = "status")
     private boolean status;
     @Column(name = "allow_comment")
     private boolean allowComment;
+
     @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "blog_user_id")
-    @JsonIgnore
     private User user;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "category_id", nullable = false)
+    @JsonIgnore
     private Category tag;
 
     public Long getId() {
@@ -50,74 +54,60 @@ public class Post {
     }
 
     public String getPostName() {
-        return postName;
-    }
-
-    public void setPostName(String postName) {
-        this.postName = postName;
-    }
-
-    public Date getPostDate() {
-        return postDate;
-    }
-
-    public void setPostDate(Date postDate) {
-        this.postDate = postDate;
-    }
-
-    public Date getLastEdit() {
-        return lastEdit;
-    }
-
-    public void setLastEdit(Date lastEdit) {
-        this.lastEdit = lastEdit;
-    }
-
-    public String getPostContent() {
-        return postContent;
-    }
-
-    public void setPostContent(String postContent) {
-        this.postContent = postContent;
-    }
-
-    public boolean isStatus() {
-        return status;
-    }
-
-    public void setStatus(boolean status) {
-        this.status = status;
-    }
-
-    public boolean isAllowComment() {
-        return allowComment;
-    }
-
-    public void setAllowComment(boolean allowComment) {
-        this.allowComment = allowComment;
-    }
-
-    public User getUser() {
-        return user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public Category getCategory() {
-        return category;
-    }
-
-    public void setCategory(Category category) {
-        this.category = category;
-    }
-
-    @Override
-    public String toString() {
-        return "Post [id=" + id + ", postName=" + postName + ", postDate=" + postDate + ", lastEdit=" + lastEdit
-                + ", postContent=" + postContent + ", status=" + status + ", allowComment=" + allowComment + ", user="
-                + user + ", category=" + category + "]";
-    }
-
+		return postName;
+	}
+	public void setPostName(String postName) {
+		this.postName = postName;
+	}
+	public Date getPostDate() {
+		return postDate;
+	}
+	public void setPostDate(Date postDate) {
+		this.postDate = postDate;
+	}
+	public Date getLastEdit() {
+		return lastEdit;
+	}
+	public void setLastEdit(Date lastEdit) {
+		this.lastEdit = lastEdit;
+	}
+	public String getPostContent() {
+		return postContent;
+	}
+	public void setPostContent(String postContent) {
+		this.postContent = postContent;
+	}
+	public boolean isStatus() {
+		return status;
+	}
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+	public boolean isAllowComment() {
+		return allowComment;
+	}
+	public void setAllowComment(boolean allowComment) {
+		this.allowComment = allowComment;
+	}
+	
+	public User getUser() {
+		return user;
+	}
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	public Category getTag() {
+		return tag;
+	}
+	public void setTag(Category tag) {
+		this.tag = tag;
+	}
+	@Override
+	public String toString() {
+		return "Post [id=" + id + ", postName=" + postName + ", postDate=" + postDate + ", lastEdit=" + lastEdit
+				+ ", postContent=" + postContent + ", status=" + status + ", allowComment=" + allowComment + ", user="
+				+ user + ", tag=" + tag + "]";
+	}
+	
 }
